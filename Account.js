bf807ea3ab6b937d7caf4801a726992a00f4b01f
Mehedi Hasan Saikat <29018897+mehedi-hasan-saikat@users.noreply.github.com>
2021-08-15 18:15:23 +0600

    FInal Version

--- Account.js
+++ Account.js
@@ -1,43 +1,59 @@
-document.getElementById('Deposite').addEventListener('click',function(){
-    const DValue =document.getElementById('DepositeAmount').value;
-    const DepositeValue= parseInt(DValue);
-    const DBalance=document.getElementById('DepositeBalance').innerText;
-    var depositebalance=parseInt(DBalance);
-    const DTotal=DepositeValue + depositebalance;
-    DepositeBalance.innerText=DTotal;
 
-    DepositeAmount.value='';
+
+
+    document.getElementById('Deposite').addEventListener('click',function(){
+        const DValue =document.getElementById('DepositeAmount').value;
+        if(DValue=='')
+        {
+            DValue=0;
+        }
+        const DepositeValue= parseInt(DValue);
+        const DBalance=document.getElementById('DepositeBalance').innerText;
+        var depositebalance=parseInt(DBalance);
+        const DTotal=DepositeValue + depositebalance;
+        DepositeBalance.innerText=DTotal;
+    
+        DepositeAmount.value='';
+        
+    
+    
+    //Total Balance Calculate
+    
+       const TBalance= document.getElementById('Balance').innerText;
+      const  tBalance=parseInt(TBalance)
+       const TotalBalance= DepositeValue + tBalance;
+       Balance.innerText=TotalBalance;
+    
+      
+       
+    })
+    
+    
+    
+    
+    document.getElementById('Withdraw').addEventListener('click',function(){
+        const WValue =document.getElementById('WithdrawAmount').value;
+        if(WValue=='')
+        {
+            WValue=0;
+        }
+        const WithdrawValue= parseInt(WValue);
+        const WBalance=document.getElementById('WithdrawBalance').innerText;
+        const withdrawbalance=parseInt(WBalance);
+        const WTotal=WithdrawValue + withdrawbalance;
+        WithdrawBalance.innerText=WTotal;
+    
+        WithdrawAmount.value='';
+    
+    // //Total Balance Calculate
+       const TBalance= document.getElementById('Balance').innerText;
+      const  tBalance=parseInt(TBalance)
+       const TotalBalance=  tBalance - WithdrawValue;
+       Balance.innerText=TotalBalance;
+    
+    
+    })
     
 
 
-//Total Balance Calculate
-   const TBalance= document.getElementById('Balance').innerText;
-  const  tBalance=parseInt(TBalance)
-   const TotalBalance= DepositeValue + tBalance;
-   Balance.innerText=TotalBalance;
 
-  
-   
-})
-
-
-
-
-document.getElementById('Withdraw').addEventListener('click',function(){
-    const WValue =document.getElementById('WithdrawAmount').value;
-    const WithdrawValue= parseInt(WValue);
-    const WBalance=document.getElementById('WithdrawBalance').innerText;
-    const withdrawbalance=parseInt(WBalance);
-    const WTotal=WithdrawValue + withdrawbalance;
-    WithdrawBalance.innerText=WTotal;
-
-    WithdrawAmount.value='';
-
-// //Total Balance Calculate
-   const TBalance= document.getElementById('Balance').innerText;
-  const  tBalance=parseInt(TBalance)
-   const TotalBalance=  tBalance - WithdrawValue;
-   Balance.innerText=TotalBalance;
-
-
-})
